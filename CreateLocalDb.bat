2e73598f3bc197badbdb511ae756fcbe3c9e98a1
Denis Andreev <dandreev@ethoslending.com>
2018-01-30 17:59:58 +0300

    Stop and remove localDb

--- CreateLocalDb.bat
+++ CreateLocalDb.bat
@@ -1,1 +1,4 @@
 "%ProgramFiles%\Microsoft SQL Server\140\Tools\Binn\SqlLocalDB.exe" create "Projects1" 14.0 -s
+"%ProgramFiles%\Microsoft SQL Server\140\Tools\Binn\SqlLocalDB.exe" info "Projects1"
+"%ProgramFiles%\Microsoft SQL Server\140\Tools\Binn\SqlLocalDB.exe" stop "Projects1"
+"%ProgramFiles%\Microsoft SQL Server\140\Tools\Binn\SqlLocalDB.exe" delete "Projects1"
